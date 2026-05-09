4ebeea84f644248e719550e14078315804a44885
TuDespensa25 <roma220130@gmail.com>
2026-05-09 12:48:30 -0400

    k

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker para Amy Nails 
 
-const CACHE_NAME = 'amynails_-v11';
+const CACHE_NAME = 'amynails_-v12';
 const urlsToCache = [
   '/amynails_/',
   '/amynails_/index.html',
